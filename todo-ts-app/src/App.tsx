0ad4c1fffc70e40aedc7dde9ce7cdfb3e6553d07
Jonathan Raya <jonathanraya79@gmail.com>
2024-02-28 14:27:17 -0600

    initial commit

---
+++ todo-ts-app/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+import {Title} from './components/Title'
+
+const App = () => {
+  return (
+    <>
+      <Title />
+
+    <div className="App">
+
+        
+        <p>
+        First Typescript application
+        
+        </p>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ todo-ts-app/src/components/Title.tsx
@@ -0,0 +1,14 @@
+import '../App.css'
+
+const Title = () => {
+    return(
+        <>
+        <div className='title'>
+            <h1> To do list</h1>
+
+            <p>Enter your items and delete them as they get completed</p>
+        </div>
+        </>
+    )
+}
+export{Title}
